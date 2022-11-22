2f2ce5fe5e1a610cecc3aa28c54b0dc48d7798c3
yoonmyunghoon <youn1791472@gmail.com>
2022-11-22 22:13:04 +0900

    아이템 17 변경 가능성을 최소화 하라.(1)

--- src/main/java/me/whiteship/chapter04/item17/part1/Person.java
+++ src/main/java/me/whiteship/chapter04/item17/part1/Person.java
@@ -1,13 +1,20 @@
 package me.whiteship.chapter04.item17.part1;
 
+/**
+ * 3. Person 은 불변 클래스이지만 내부적으로 가변 컴포넌트(Address) 를 가지고 있음
+ */
 public final class Person {
 
-    private final Address address;
+    private final Address address; // 가변 컴포넌트
 
     public Person(Address address) {
         this.address = address;
     }
 
+    /**
+     * 4. 가변 컴포넌트에 접근할 수 없도록 getter 를 제공하지 않거나,
+     *    꼭 제공해야한다면 방어적 복사로 제공해야한다.
+     */
     public Address getAddress() {
         Address copyOfAddress = new Address();
         copyOfAddress.setStreet(address.getStreet());
@@ -25,6 +32,6 @@ public final class Person {
         Address redmond = person.getAddress();
         redmond.setCity("Redmond");
 
-        System.out.println(person.address.getCity());
+        System.out.println(person.address.getCity()); // 변경되지 않았음
     }
 }
--- src/main/java/me/whiteship/chapter04/item17/part1/PhoneNumber.java
+++ src/main/java/me/whiteship/chapter04/item17/part1/PhoneNumber.java
@@ -1,5 +1,18 @@
 package me.whiteship.chapter04.item17.part1;
 
+/**
+ * 1. 불변 클래스
+ *      - 객체가 생성되서 소멸될 때까지 상태가 변하지 않도록 설계한 클래스
+ *      - 설계, 구현, 사용하기 쉬우며, 오류가 생길 여지가 적고 안전하다.
+ *      - 스레드 세이프하다.
+ *      - 인스턴스를 캐싱해두고 여러 스레드가 사용할 수 있다. -> 성능상 이점
+ * 2. 불변 클래스 만드는 다섯가지 규칙
+ *      - 객체 상태 변경 메서드(setter)를 제공하지 않는다.
+ *      - 상속(클래스 확장)을 할 수 없도록 한다. -> final or only private 생성자
+ *      - 모든 필드를 final 로 선언한다.
+ *      - 모든 필드를 private 으로 선언한다.
+ *      - 자신 외에는 내부의 가변 컴포넌트에 접근할 수 없도록 한다.
+ */
 public final class PhoneNumber {
 
     private final short areaCode, prefix, lineNum;
