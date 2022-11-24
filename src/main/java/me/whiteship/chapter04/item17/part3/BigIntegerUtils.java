afbc99b8dd1bfe8c0954f1025596ddd884c725ae
reynold-yoon <reynold.yoon@kakaocorp.com>
2022-11-24 16:59:54 +0900

    아이템 17 변경 가능성을 최소화 하라.(수정)

--- src/main/java/me/whiteship/chapter04/item17/part3/BigIntegerUtils.java
+++ src/main/java/me/whiteship/chapter04/item17/part3/BigIntegerUtils.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class BigIntegerUtils {
 
     /**
-     * 8. 가변 클래스인데 상속이 가능한 경우
+     * 8. 불변 클래스인데 상속이 가능한 경우
      *      - 매개변수로 들어온 val 가 불변인지 확실하지 않음
      *      - 방어적 복사를 사용해야 한다. BigInteger 인스턴스를 새로 만들어서 제공
      */
